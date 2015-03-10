845b66250d52f0ea51d68b91fa653ce0682dcee9
StumblinGrumblin <gary@sweetlabs.com>
2015-03-10 15:41:41 -0700

    Posts/Comments models and controllers built

---
+++ app/models/comment.rb
@@ -0,0 +1,22 @@
+class Comment < ActiveRecord::Base
+  belongs_to :post
+
+  def create
+    post = Post.find(params[:post_id])
+    comment = post.comments.create(comment_params)
+    respond_with post, comment
+  end
+
+  def upvote
+    post = Post.find(params[:post_id])
+    comment = post.comments.find(params[:id])
+    comment.increment!(:upvotes)
+
+    respond_with post, comment
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
